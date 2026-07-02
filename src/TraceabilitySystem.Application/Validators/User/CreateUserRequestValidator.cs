1371b74cd272486145ae95c938fe8889d442cd46
Agung Kusaeri <agung.kusaeri9@gmail.com>
2026-07-02 09:19:50 +0700

    refactor(auth): remove complex password validation requirements

--- src/TraceabilitySystem.Application/Validators/User/CreateUserRequestValidator.cs
+++ src/TraceabilitySystem.Application/Validators/User/CreateUserRequestValidator.cs
@@ -15,10 +15,6 @@ public class CreateUserRequestValidator : AbstractValidator<CreateUserRequest>
             .NotEmpty().WithMessage("Username is required.");
 
         RuleFor(x => x.Password)
-            .NotEmpty().WithMessage("Password is required.")
-            .MinimumLength(8).WithMessage("Password must be at least 8 characters.")
-            .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
-            .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter.")
-            .Matches(@"\d").WithMessage("Password must contain at least one number.");
+            .NotEmpty().WithMessage("Password is required.");
     }
 }
